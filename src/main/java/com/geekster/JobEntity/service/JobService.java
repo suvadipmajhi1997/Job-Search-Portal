c0cb1508609591752cb0d4d55bab9f987451342a
suvadip majhi <114084926+suvadipmajhi1997@users.noreply.github.com>
2023-05-02 11:41:35 +0530

    custom query modified

--- src/main/java/com/geekster/JobEntity/service/JobService.java
+++ src/main/java/com/geekster/JobEntity/service/JobService.java
@@ -45,4 +45,15 @@ public class JobService {
     public List<Job> getJobsBySalaryGreaterThanEqual(Double salary) {
         return jobRepository.findBySalaryGreaterThanEqual(salary);
     }
+      public List<Job> findJobsByEmployerName(String employerName) {
+        return jobRepository.findJobsByEmployerName(employerName);
+    }
+
+    public void updateTitleById(Long id) {
+       jobRepository.updateTitleById(id);
+    }
+
+    public void deleteById(Long id) {
+        jobRepository.deleteById(id);
+    }
 }
